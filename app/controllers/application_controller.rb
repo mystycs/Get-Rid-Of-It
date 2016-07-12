421ac5b76025d2fa12cd6e01953b469bac54a93c
Mark Stein <mystycs@gmail.com>
2016-07-12 02:57:36 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "meowmix"
+    set :session_secret, "kwyjibo"
   end
 
 
