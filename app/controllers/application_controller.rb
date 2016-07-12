55fb630ef97825a2db1ba7d0040336949cca3b06
Mark <mystycs@gmail.com>
2016-07-12 02:11:07 -0400

    Added Base Sinatra

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "meowmix"
+  end
+
+
+  get '/' do
+    erb :index
+  end
+
+end
