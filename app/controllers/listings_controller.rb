0ab62017422e751df8401254d44f4a6a172372de
Mark <mystycs@gmail.com>
2016-07-13 17:07:09 -0400

    added submit listing form + display listing on /listings/

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -12,4 +12,14 @@ class ListingsController < ApplicationController
     erb :'listings/new'
   end
 
+  post "/listings" do
+  redirect_if_not_logged_in
+
+  unless Listing.valid_params?(params)
+    redirect "/listings/new?error=invalid posting"
+  end
+  Listing.create(params)
+  redirect "/listings"
+end
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
 
+  def self.valid_params?(params)
+    return !params[:title].empty? && !params[:price].empty? && !params[:location].empty?
+  end
+
 end
