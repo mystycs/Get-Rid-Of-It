859048081ba5e682c82ee45e427c9eb92840c90a
Mark <mystycs@gmail.com>
2016-07-13 02:07:11 -0400

    added user_id to listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713055516) do
+ActiveRecord::Schema.define(version: 20160713060623) do
 
   create_table "listings", force: :cascade do |t|
-    t.string "title"
-    t.float  "price"
-    t.string "location"
-    t.text   "description"
+    t.string  "title"
+    t.float   "price"
+    t.string  "location"
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
