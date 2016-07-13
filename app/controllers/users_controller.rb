5ece88ffda3a3d0b095d1790604aea4cecfdddf6
Mark <mystycs@gmail.com>
2016-07-13 18:44:03 -0400

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
 
-  get '/users/:id' do
-  if !logged_in?
-    redirect '/listings'
-  end
-
-  # @user = User.find(params[:id])
-  # if !@user.nil? && @user == current_user
-  #   erb :'users/show'
-  # else
-  #   redirect '/listings'
-  # end
-end
+#   get '/users/:id' do
+#   if !logged_in?
+#     redirect '/listings'
+#   end
+#
+#   # @user = User.find(params[:id])
+#   # if !@user.nil? && @user == current_user
+#   #   erb :'users/show'
+#   # else
+#   #   redirect '/listings'
+#   # end
+# end
 
   get '/signup' do
     if !session[:user_id]
