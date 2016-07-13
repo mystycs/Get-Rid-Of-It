db2e6fb385710354312ad484ad809aa0ccd96175
Mark <mystycs@gmail.com>
2016-07-13 02:28:43 -0400

    listings modifications to controller/view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,7 +2,14 @@ class ListingsController < ApplicationController
 
   get "/listings" do
     redirect_if_not_logged_in
+    @listings = Listing.all
     erb :'listings/index'
+  end
+
+  get "/listings/new" do
+    redirect_if_not_logged_in
+    @error_message = params[:error]
+    erb :'listings/new'
   end
 
 end
